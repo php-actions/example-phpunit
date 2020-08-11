93a2a4d8d651255334364943c0276edc561f2edb
Greg Bowler <greg.bowler@g105b.com>
2020-08-11 15:36:05 +0100

    Spacing

--- test/GreeterTest.php
+++ test/GreeterTest.php
@@ -6,6 +6,9 @@ use App\Greeter;
 class GreeterTest extends \PHPUnit\Framework\TestCase {
 	public function testGreeterSaysHello() {
 		$greeter = new Greeter();
-		self::assertStringContainsString("Hello", $greeter->greet());
+		self::assertStringContainsString(
+			"Hello",
+			$greeter->greet()
+		);
 	}
 }
