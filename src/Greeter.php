e77a061ecd52287e8e1f2bf8d349326ee07712d0
Luciano Ruiz Morales <lucianoruizmorales@gmail.com>
2022-01-26 11:15:57 -0300

    test

--- src/Greeter.php
+++ src/Greeter.php
@@ -3,6 +3,14 @@ namespace App;
 
 class Greeter {
 	public function greet(string $name = null):string {
+		$str = "Bob is a Bird... Bob is a Plane... Bob is Superman!";
+		$changed = preg_replace("/Bob is/", "It's", $str); // Noncompliant
+		$changed = preg_replace("/\.\.\./", ";", $changed);
+
+		$str = "Bob is a Bird... Bob is a Plane... Bob is Superman!";
+		$changed = str_replace("Bob is", "It's", $str);
+		$changed = str_replace("...", ";", $changed);
+
 		if($name) {
 			return "Hello, $name!";
 		}
